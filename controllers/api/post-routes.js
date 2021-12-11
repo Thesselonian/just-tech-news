c743970194a457173ca1dee41673d856f9627f42
Eric <ericyoung111@gmail.com>
2021-12-11 11:10:39 -0700

    made comment and upvote functionality and conditionally display login/logout buttons based on logged in status

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -89,14 +89,17 @@ router.post('/', (req, res) => {
     });
 });
 
+//upvote a post
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+  //make sure the session exists first
+  if (req.session.username) {
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+  }
 });
 
 router.put('/:id', (req, res) => {
--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -5,7 +5,7 @@ async function logout() {
     });
 
     if (response.ok) {
-        document.location.replace('.');
+        document.location.replace('/');
     } else {
         alert(response.statusText);
     }
